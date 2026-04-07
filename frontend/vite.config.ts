6568fe42d37db9163f740bf00fa1af27bf31ec3e
angelhalo10000 <angelhalo10000@gmail.com>
2026-04-07 12:56:03 +0900

    docker環境調整

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// const backendUrl = process.env.BACKEND_URL ?? 'http://localhost:3000'
+const backendUrl = process.env.BACKEND_URL ?? 'http://host.docker.internal:3000'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: backendUrl,
         changeOrigin: true,
       },
       '/rails': {
-        target: 'http://localhost:3000',
+        target: backendUrl,
         changeOrigin: true,
       },
     },
